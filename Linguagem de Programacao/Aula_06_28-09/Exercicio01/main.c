aa18c517d6b4641c7e03d5afdce0b198e4b75dc0
YhonathanPavan <yhonatannpavan@gmail.com>
2021-10-25 12:13:32 -0300

    adicionando Readme

--- Linguagem de Programacao/Aula_06_28-09/Exercicio01/main.c
+++ Linguagem de Programacao/Aula_06_28-09/Exercicio01/main.c
@@ -1,32 +1,43 @@
 #include <stdio.h>
 #include <stdlib.h>
-#define T 10
+#define F 4
+#define E 8
 
-int main()
-{
-int vetorA[T];
-int vetorB[T];
-int i;
+int main(){
 
-for(i = 1; i <= T; i++){
-    printf("Insira um valor: \n");
-    scanf("%d", &vetorA[i]);
+int ra[E], notaUm[F], notaDois[F], notaTres[F], notaQuatro[F];
+int i, j, soma[E];
+float media[E];
 
-        if(i % 2 == 0){
-            vetorB[i] = i * 5;
-        }else{
-            vetorB[i] = i + 5;
+    for(i = 1; i <= 2; i++){
+    printf("Insira o numero do RA do aluno %d : \n", i);
+    scanf("%d", &ra[i]);
+        for(j = 1; j <= 2; j++){
+            printf("Muito bem, agora insira a nota do 1 bimestre desse aluno: \n");
+             scanf("%d", &notaUm[j]);
+            printf("Muito bem, agora insira a nota do 2 bimestre desse aluno: \n");
+             scanf("%d", &notaDois[j]);
+            printf("Muito bem, agora insira a nota do 3 bimestre desse aluno: \n");
+             scanf("%d", &notaTres[j]);
+            printf("Muito bem, agora insira a nota do 4 bimestre desse aluno: \n");
+             scanf("%d", &notaQuatro[j]);
         }
-}
-printf("\nVetor A: ");
+    soma[i] = soma[i] + notaUm[j] + notaDois[j] + notaTres[j] + notaQuatro[j];
+    media[i] = soma[i] / E;
+    }
 
-for(i = T; i >= 1; i--){
-    printf("%3d |", vetorA[i]);
-}
-printf("\nVetor B: ");
-for(i = T; i >= 1; i--){
-    printf("%3d |", vetorB[i]);
-}
-printf("\n\n");
+    printf("\n");
+
+     for(i = 1; i <= E; i++){
+        printf("RA: %9d |");
+            for(j = 1; j <= F; j++){
+                printf("Nota 1 Bim: %2d | \n", notaUm[j]);
+                printf("Nota 2 Bim: %2d | \n", notaDois[j]);
+                printf("Nota 3 Bim: %2d | \n", notaTres[j]);
+                printf("Nota 4 Bim: %2d | \n", notaQuatro[j]);
+            }
+        printf("Media %d eh: %.2f  |", media[i]);
+     }
+
     return 0;
 }
---
+++ Linguagem de Programacao/README.md
@@ -0,0 +1,24 @@
+# Exercícios feitos em aula - Algoritmo e Lógica de programação.
+
+Olá! Com o passar dos dias na universidade, fui resolvendo alguns exercícios e desafios que foram propostos em aula. Esse diretório é referente ao meu desenvolvimento na matéria de "Linguagem de programação", com somente a resolução dos exercícios, sem os enunciados.
+
+(Todos os exercícios foram resolvidos em Linguagem C)
+
+## Conteúdos que aprendi: 
+- Tipos de dados, Identificadores, declaração de variáveis, palavras reservadas, operações básicas, comandos de entrada e saída;
+
+- Estruturas de controle de fluxo (seleção);
+
+- Estruturas de repetição – iteração e comandos de desvio: break e continue, expressão e bloco;
+
+- Matrizes e Strings;
+
+- Procedimentos e Funções;
+
+- Funções Recursivas;
+
+- Ponteiros;
+
+- Estruturas (Structs) ou Registros;
+
+##      
